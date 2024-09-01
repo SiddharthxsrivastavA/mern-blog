05ddca10f4b8179c10e6b2b493a79331e08d48f2
Siddharth Srivastava <120250296+SiddharthxsrivastavA@users.noreply.github.com>
2024-09-01 10:14:39 +0530

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -48,8 +48,7 @@ export default function SignUp() {
             Blog
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
-            or with Google.
+            This is a demo project. You can sign up with your email and password.
           </p>
         </div>
         {/* right */}
